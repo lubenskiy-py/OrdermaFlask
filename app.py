4b3a59417bc767802d99c3daaf9951f2871dd9b1
lupenskiy-py <ivanlubenskiy@gmail.com>
2024-12-12 15:02:08 -0300

    add navigation

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from models import db, Pizza
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db.init_app(app)
+
+@app.route("/homepage")
+def homepage():
+    return render_template("homepage.html", title='Orderman')
+
+@app.route("/menu")
+def menu():
+    pizzas = Pizza.query.all()
+    context = {
+        'title': 'Меню',
+        'pizzas': pizzas,
+    }
+    return render_template("menu.html", **context)
+
+@app.route("/navigation")
+def navigation():
+    return render_template("navigation.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
